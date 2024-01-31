b25e54ff396bc1ecb5b1770c046a8bd5c1000b02
Vaishnavi Thakre <86373178+VaishnaviThakre@users.noreply.github.com>
2024-01-31 18:39:07 +0530

    Add files via upload

---
+++ comparisionOperator.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first value:"))
+b = int(input("Enter second value:"))
+
+if a > b:
+    print(a, "is greater than", b)
+elif a < b:
+    print(b, "is greater than", a)
+elif a ==b:
+    print(a, "is equal to ", b)
